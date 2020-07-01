479fa3d80e37a1b5f326a9a81069b1579923246f
dcentelles <centelld@uji.es>
2020-07-01 20:44:42 +0200

    current status

--- cronscripts/update_devlist.sh
+++ cronscripts/update_devlist.sh
@@ -12,13 +12,14 @@ then
 	cd /var/www/html/snpservices/graphs
 	echo "~~~~ START ~~~~ $(date '+\%Y-\%m-\%d \%H:\%M:\%S')" >> /var/log/snpservices/mrtgccfg.log 2>&1
 
-#	wget -O ../data/cnml/poblatornesa.cnml https://guifi.net/ca/guifi/cnml/20887/detail >> /var/log/snpservices/mrtgccfg.log 2>&1
-#	wget -O ../data/cnml/castellodelaplana.cnml https://guifi.net/ca/guifi/cnml/11275/detail >> /var/log/snpservices/mrtgccfg.log 2>&1
+	wget -O ../data/cnml/poblatornesa.cnml https://guifi.net/ca/guifi/cnml/20887/detail >> /var/log/snpservices/mrtgccfg.log 2>&1
+	wget -O ../data/cnml/castellodelaplana.cnml https://guifi.net/ca/guifi/cnml/11275/detail >>  /var/log/snpservices/mrtgccfg.log 2>&1
+	
+	if [ -x /usr/bin/php ] && [ -r /var/www/html/snpservices/graphs/mrtgcsv2mrtgcfg.php ]
+	then env LANG=C /usr/bin/php mrtgcsv2mrtgcfg.php >> /var/log/snpservices/mrtgccfg.log 2>&1
+		echo "Interval: 10" >> ../data/mrtg.cfg
+	fi
 
-fi
-if [ -x /usr/bin/php ] && [ -r /var/www/html/snpservices/graphs/mrtgcsv2mrtgcfg.php ]
-then env LANG=C /usr/bin/php mrtgcsv2mrtgcfg.php >> /var/log/snpservices/mrtgccfg.log 2>&1
 	echo "~~~~ END ~~~~ $(date '+\%Y-\%m-\%d \%H:\%M:\%S')" >> /var/log/snpservices/mrtgccfg.log 2>&1
-	echo "Interval: 10" >> ../data/mrtg.cfg
 fi
 
